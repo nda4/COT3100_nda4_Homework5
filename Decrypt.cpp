6a21391bf3550f0803b7bca472f1a27fc7bcdf6f
nda4 <nda4@students.uwf.edu>
2021-11-21 22:27:44 -0600

    Final unworking push. Can't seem to figure the negative answer for some reason... oh well

--- Decrypt.cpp
+++ Decrypt.cpp
@@ -3,64 +3,74 @@
 #include <math.h>
 
 /*
-    ***IN THE TERMINAL TYPE "make" AND THE PROGRAME WILL RUN***
+    ***IN THE TERMINAL TYPE "make" AND THE PROGRAM WILL RUN***
 
-    I wrote prime generator cause im dumb and thought i would need to...zzz
+    I am definitely doing something wrong...but i am not sure what.
+    It works just fine for most values. But for the values provided in
+    the homework i am recieving a negative value and i simply cannot figure
+    out why. My guess it has something to do with larbe values but I am not sure.
 */
 
 
-int findPrivate(int p, int q);
-int generatePrime();
-bool isPrime(int n);
+long findPrivate(long p, long phi_n, long l);
+long lcm(long a, long b);
+long generatePrime(long n);
+bool isPrime(long n);
+long gcd(long a, long b);
+
 
 int main(){
-    int n, e, d;
-    std::string encrypted_sentence;
+    long n, e, p, q, phi_e;                                       
+long d;                                                                
+    long encrypted_sentence;
 
     std::cout << "Please enter the n value: ";
     std::cin >> n;
     std::cout << "Please enter the e value: ";
     std::cin >> e;
-    d = findPrivate(e, n);
-    std::cout << "\nPublic Key: {" << e << "," << n << "}\nPrivate Key: {" << d << "," << n << "}"; 
+
+    p = generatePrime(n);
+    q = n/p;
+    long l = lcm(p - 1, q - 1);
+    phi_e = (p-1)*(q-1);
+
+    d = findPrivate(e, phi_e, l);
+    std::cout << "\nPublic Key: {" << e << "," << n << "}\nPrivate Key: {" << d << "," << n << "}\n\n"; 
     
     std::cout << "\n\nPlease type in the value encrypted with the Public Key: ";
     std::cin >> encrypted_sentence;
 
-    for(int i = 0; i > encrypted_sentence.size(); i++){
-        encrypted_sentence.at(i) =  fmod(pow(encrypted_sentence.at(i), e), (float)n);
-    }
-
-    std::cout << encrypted_sentence << std::endl;
+    long decrypted_sentence = (long)pow(encrypted_sentence, d)%n; 
+    std::cout << "Unencrypted Value: " << decrypted_sentence << std::endl;
+    return 0;
 }
 
 //Returns d (the private key)
-int findPrivate(int e, int n){
-    float d = (((float)n*1) + 1)/(float)e;
-    int i = 1;
-    while((fmod(d, 1.0)) != 0){
-        i++;
-        d =(((float)n*(float)i) + 1.0)/(float)e;
+long findPrivate(long e, long phi_e, long l){
+    for(long i = 1; i < phi_e; i++){
+        if((e*i) % l == 1 % l){
+            return i;
+        }
     }
-    return d;
+    return 0;
 }
 
+
+
 // Gives a random prime number between given range
-int generatePrime(){
-    int current_number = 2;
-    int current_prime = current_number;
-    while(current_number <= (rand() % 8011 + 997)){
-        current_number++;
-        if(isPrime(current_number))
-            current_prime = current_number;
-    }
-    return current_prime;
+long generatePrime(long n){
+    for(long i = 2; i <= n; i++){
+        if(n % i == 0 && isPrime(i))
+            return i;
+        }
+    return 1;
 }
+
 //Returns bool true if num is prime, false if not
-bool isPrime(int n) {
-   int i, flag = 0;
+bool isPrime(long n) {
+   long i, flag = 0;
    for(i=2; i<=n/2; ++i) {
-      if(n%i==0) {
+      if(n % i==0) {
          flag=1;
          break;
       }
@@ -69,4 +79,30 @@ bool isPrime(int n) {
         return true;
     else
         return false;
+}
+
+long gcd(long a, long b) {
+   long t;
+   while(t != 0) {
+        t= a % b;
+        if(t==0)
+            return b;
+        a = b;
+        b= t;
+   }
+   return 0;
+}
+
+long lcm(long a, long b){
+    long max = 1;
+    do
+    {
+        if (max % a == 0 && max % b == 0)
+        {
+            return max;
+            break;
+        }
+        else
+            ++max;
+    } while (true);
 }
